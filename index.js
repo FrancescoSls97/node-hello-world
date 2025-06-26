13a9a65f22838b6a8fbd3c58c28a84669738a084
FrancescoSls97 <frankds1997@gmail.com>
2025-06-26 21:30:50 +0200

    add ajax call via axios

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 import chalk from "chalk";
+import axios from "axios";
 
 console.log("Hello Boolean");
 console.log(process.env.super_secret_key);
 console.log(chalk.red("Red Log"));
+
+axios
+  .get("https://lanciweb.github.io/demo/api/pictures/")
+  .then((response) => {
+    //codice da eseguire se la richiesta AJAX ha successo
+    console.log(response.data);
+  })
+  .catch((error) => {
+    //se la richiesta AJAX fallisce
+    console.error(error);
+  });
